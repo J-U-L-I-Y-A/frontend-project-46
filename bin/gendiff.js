163710f1b8e8b35ab2863c487d2824213a94a46e
Юлия Красулина <juliya94m@gmail.com>
2023-04-18 22:13:56 +0300

    step 3

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import { program } from 'commander';
+import genDiff from "../src/index.js";
 
 program
     .name('gendiff')
@@ -9,6 +10,9 @@ program
     .arguments('<filepath1> <filepath2>')
     .option('-f, --format <type>', 'output format', 'stylish')
     .action((filepath1, filepath2) => {
+       const diff = genDiff(filepath1, filepath2);
+       console.log(diff);
     });
 
 program.parse();
+p
--- src/index.js
+++ src/index.js
@@ -1,2 +1,39 @@
+import fs from 'fs';
+import path from 'path';
+import _ from "lodash";
 
+const getDataFromFile = (filepath) => {
+    const fullPath = path.resolve(process.cwd(), filepath);
+    return fs.readFileSync(fullPath, 'utf-8');
+};
 
+// const getExtension = path.extname(filepath);
+
+const parsedJsonInObj = (data) => JSON.parse(data);
+
+const genDiff = (filepath1, filepath2) => {
+    const data1 = getDataFromFile(filepath1);
+    const data2 = getDataFromFile(filepath2);
+    const obj1 = parsedJsonInObj(data1);
+    const obj2 = parsedJsonInObj(data2);
+    const keys1 = Object.keys(obj1);
+    const keys2 = Object.keys(obj2);
+    const unionKeys = _.union(keys1, keys2);
+    const sortedKeys = _.sortBy(unionKeys);
+
+    const result = sortedKeys.reduce((acc, key) => {
+        if (!_.has(obj1, key)) {
+            acc.push(`+ ${key}: ${obj2[key]}\n`);
+        } else if (!_.has(obj2, key)) {
+            acc.push(`- ${key}: ${obj1[key]}\n`);
+        } else if (obj1[key] === obj2[key]) {
+            acc.push(`  ${key}: ${obj1[key]}\n`);
+        } else if (obj1[key] !== obj2[key]) {
+            acc.push(`- ${key}: ${obj1[key]}\n`);
+            acc.push(`+ ${key}: ${obj2[key]}\n`);
+        }
+        return acc;
+    }, []);
+    return `{\n${result.join('')}\n}`;
+};
+export default genDiff;
