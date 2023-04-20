563223e5ec2112540298b33bd5a20038ee6c3ec1
Юлия Красулина <juliya94m@gmail.com>
2023-04-20 16:01:34 +0300

    step 4

---
+++ Makefile
@@ -0,0 +1,17 @@
+# Makefile
+install:
+	npm ci
+
+gendiff:
+	node bin/gendiff.js
+
+publish:
+	npm publish --dry-run
+
+lint:
+	npx eslint .
+
+test-coverage:
+	npm test -- --coverage --coverageProvider=v8
+
+.PHONY: test
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,18 +1,17 @@
 #!/usr/bin/env node
 
 import { program } from 'commander';
-import genDiff from "../src/index.js";
+import genDiff from '../src/index.js';
 
 program
-    .name('gendiff')
-    .description('Compares two configuration files and shows a difference.')
-    .version('10.0.1')
-    .arguments('<filepath1> <filepath2>')
-    .option('-f, --format <type>', 'output format', 'stylish')
-    .action((filepath1, filepath2) => {
-       const diff = genDiff(filepath1, filepath2);
-       console.log(diff);
-    });
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('10.0.1')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .action((filepath1, filepath2) => {
+    const diff = genDiff(filepath1, filepath2);
+    console.log(diff);
+  });
 
 program.parse();
-p
