265de655e6b933ff3b662336377aec2642d49ff3
Юлия Красулина <juliya94m@gmail.com>
2023-05-13 16:35:04 +0300

    step 8

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -10,14 +10,22 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-test('json files compare', () => {
-  const expected = readFile('expectedCompareJSON.txt');
-  const actual = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'));
-  expect(actual).toBe(expected);
+test('json', () => {
+  const file1 = getFixturePath('file1.json');
+  const file2 = getFixturePath('file2.json');
+
+  expect(genDiff(file1, file2)).toEqual(readFile('expectedStylish.txt'));
+  expect(genDiff(file1, file2, 'stylish')).toEqual(readFile('expectedStylish.txt'));
+  expect(genDiff(file1, file2, 'plain')).toEqual(readFile('expectedPlain.txt'));
+  expect(genDiff(file1, file2, 'json')).toEqual(readFile('JSON.txt'));
 });
 
-test('yaml files compare', () => {
-  const expected = readFile('expectedCompareYAML.txt');
-  const actual = genDiff(getFixturePath('file1.yaml'), getFixturePath('file2.yaml'));
-  expect(actual).toBe(expected);
+test('yaml/yml', () => {
+  const file3 = getFixturePath('file1.yaml');
+  const file4 = getFixturePath('file2.yaml');
+
+  expect(genDiff(file3, file4)).toEqual(readFile('expectedStylish.txt'));
+  expect(genDiff(file3, file4, 'stylish')).toEqual(readFile('expectedStylish.txt'));
+  expect(genDiff(file3, file4, 'plain')).toEqual(readFile('expectedPlain.txt'));
+  expect(genDiff(file3, file4, 'json')).toEqual(readFile('expectedJSON.txt'));
 });
--- src/index.js
+++ src/index.js
@@ -1,40 +1,22 @@
 import fs from 'fs';
 import path from 'path';
-import _ from 'lodash';
-import parse from './parses.js';
-
-const getDataFromFile = (filepath) => {
-  const fullPath = path.resolve(process.cwd(), filepath);
-  return fs.readFileSync(fullPath, 'utf-8');
-};
+import parse from './parsers.js';
+import buildTree from './buildTree.js';
+import formatData from './formatters/index.js';
 
 const getFormat = (filepath) => path.extname(filepath).slice(1);
 
-const genDiff = (filepath1, filepath2) => {
-  const data1 = getDataFromFile(filepath1);
-  const data2 = getDataFromFile(filepath2);
-  const format1 = getFormat(filepath1);
-  const format2 = getFormat(filepath2);
-  const obj1 = parse(data1, format1);
-  const obj2 = parse(data2, format2);
-  const keys1 = Object.keys(obj1);
-  const keys2 = Object.keys(obj2);
-  const unionKeys = _.union(keys1, keys2);
-  const sortedKeys = _.sortBy(unionKeys);
-
-  const result = sortedKeys.reduce((acc, key) => {
-    if (!_.has(obj1, key)) {
-      acc.push(`  + ${key}: ${obj2[key]}\n`);
-    } else if (!_.has(obj2, key)) {
-      acc.push(`  - ${key}: ${obj1[key]}\n`);
-    } else if (obj1[key] === obj2[key]) {
-      acc.push(`    ${key}: ${obj1[key]}\n`);
-    } else if (obj1[key] !== obj2[key]) {
-      acc.push(`  - ${key}: ${obj1[key]}\n`);
-      acc.push(`  + ${key}: ${obj2[key]}\n`);
-    }
-    return acc;
-  }, []);
-  return `{\n${result.join('')}}`;
+const getData = (filepath) => {
+  const fullPath = path.resolve(process.cwd(), filepath);
+  return parse(fs.readFileSync(fullPath, 'utf-8'), getFormat(filepath));
 };
+
+const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
+  const data1 = getData(filepath1);
+  const data2 = getData(filepath2);
+  const tree = buildTree(data1, data2);
+
+  return formatData(tree, formatName);
+};
+
 export default genDiff;
